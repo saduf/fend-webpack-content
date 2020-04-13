d5650a4ba2271af96b4daa00b3469a1bf3982a03
Fernando <fernandosadu@gmail.com>
2020-04-12 23:51:38 -0400

    adding webpack loaders and plugins

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,8 @@
+import { checkForName } from './js/nameChecker'
+import { handelSubmit } from './js/formHandler'
+
+import { functionName } from './js/formHandler'
+
+console.log(checkForName);
+
 alert("I exist!");
